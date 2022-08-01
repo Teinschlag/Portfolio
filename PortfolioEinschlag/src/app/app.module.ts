83986f5a7aee011f7aba7ee93fc550dfda63d562
Tomi <tomi94.lp@gmail.com>
2022-08-01 16:47:13 -0300

    Corrección de errores

--- PortfolioEinschlag/src/app/app.module.ts
+++ PortfolioEinschlag/src/app/app.module.ts
@@ -4,14 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { APComponent } from './components/ap/ap.component';
-import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component'; 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    APComponent,
     LogoAPComponent
   ],
   imports: [
