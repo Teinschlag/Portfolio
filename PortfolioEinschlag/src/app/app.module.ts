bff0d6ddb45df93224347d400ccdb3797bc4aa85
Tomi <tomi94.lp@gmail.com>
2022-08-01 14:55:22 -0300

    ...

--- PortfolioEinschlag/src/app/app.module.ts
+++ PortfolioEinschlag/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { APComponent } from './components/ap/ap.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    APComponent,
+    LogoAPComponent
   ],
   imports: [
     BrowserModule,
