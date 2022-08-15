627cf44e362503376962d1caea63d65fb5047b73
Tomi <tomi94.lp@gmail.com>
2022-08-15 17:25:37 -0300

    Cree carpeta BackEnd

---
+++ PortfolioEinschlag/BackEnd/src/main/java/com/portfolio/teins/TeinsApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.teins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeinsApplication.class, args);
+	}
+
+}
---
+++ PortfolioEinschlag/BackEnd/src/test/java/com/portfolio/teins/TeinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.teins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
