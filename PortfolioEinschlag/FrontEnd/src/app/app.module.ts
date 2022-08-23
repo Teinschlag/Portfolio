c7ed2a1d7b1e284bf4677a0b1f488b861242de38
Tomi <tomi94.lp@gmail.com>
2022-08-23 16:22:26 -0300

    Formulario
    Ahora anda y redirecciona bien

--- PortfolioEinschlag/FrontEnd/src/app/app.module.ts
+++ PortfolioEinschlag/FrontEnd/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 @NgModule({
@@ -34,8 +35,10 @@ import { LoginComponent } from './components/login/login.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgCircleProgressModule.forRoot({})
-  ],
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
+
+  ],   
   providers: [],
   bootstrap: [AppComponent]
 })
